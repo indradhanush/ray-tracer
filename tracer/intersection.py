1f0ad4120667c9ec108537070abf74e7aeda2676
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2017-07-17 19:10:45 -0400

    Move trace inside Sphere

--- tracer/intersection.py
+++ tracer/intersection.py
@@ -16,6 +16,37 @@ class Sphere():
         self.centre = centre
         self.radius = radius
 
+    @staticmethod
+    def is_valid_root(root):
+        if isinstance(root, np.complex128):
+            return False
+
+        if root < 0:
+            return False
+
+        return True
+
+    def trace(self, ray):
+        px = ray.direction.x
+        py = ray.direction.y
+        pz = ray.direction.z
+
+        a = (px ** 2) + (py ** 2) + (pz ** 2)
+
+        qx = (ray.start.x - self.centre.x)
+        qy = (ray.start.y - self.centre.y)
+        qz = (ray.start.z - self.centre.z)
+
+        b = 2 * ((px * qx) + (py * qy) + (pz * qz))
+
+        c = (qx ** 2) + (qy ** 2) + (qz ** 2) - (self.radius ** 2)
+
+        roots = np.roots([c, b, a])
+        if any(self.is_valid_root(root) for root in roots) is True:
+            return True
+        else:
+            return False
+
 
 class ImagePlane():
     def __init__(self, width, height):
@@ -38,44 +69,10 @@ class ImagePlane():
             radius=200
         )
 
-    @staticmethod
-    def is_valid_root(root):
-        if isinstance(root, np.complex128):
-            return False
-
-        if root < 0:
-            return False
-
-        return True
-
-    def trace_sphere(self, ray, sphere):
-        centre = sphere.centre
-
-        px = ray.direction.x
-        py = ray.direction.y
-        pz = ray.direction.z
-
-        a = (px ** 2) + (py ** 2) + (pz ** 2)
-
-        qx = (ray.start.x - centre.x)
-        qy = (ray.start.y - centre.y)
-        qz = (ray.start.z - centre.z)
-
-        b = 2 * ((px * qx) + (py * qy) + (pz * qz))
-
-        c = (qx ** 2) + (qy ** 2) + (qz ** 2) - (sphere.radius ** 2)
-
-        roots = np.roots([c, b, a])
-        if any(self.is_valid_root(root) for root in roots) is True:
-            return True
-        else:
-            return False
-
     def render_pixel(self, row, col, pixel, sphere):
         ray = Ray(self.origin, pixel)
 
-        result = self.trace_sphere(ray, sphere)
-
+        result = sphere.trace(ray)
         if result is True:
             color = 255
         elif result is False:
